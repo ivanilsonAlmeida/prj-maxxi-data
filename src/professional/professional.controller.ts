4c9f19255dfc96143a6c99a3f36c2803a07d4a19
ivanilsonAlmeida <ivan.almeidapibane@gmail.com>
2021-04-29 23:11:11 -0300

    create from professional structure

---
+++ src/professional/professional.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get, Post, Put } from '@nestjs/common';
+import { ProfessionalService } from './professional.service';
+
+@Controller('')
+export class ProfessionalController {
+  constructor(private readonly _service: ProfessionalService) {}
+
+  @Post('/professional')
+  public createProfessional() {
+    return null;
+  }
+
+  @Put('/professional')
+  public editProfessional() {
+    return null;
+  }
+
+  @Get('/professional')
+  public findProfessional() {
+    return null;
+  }
+}
---
+++ src/professional/professional.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class ProfessionalService {
+  constructor() {}
+
+  public saveProfessional() {
+    return null;
+  }
+
+  public editProfessional() {
+    return null;
+  }
+
+  public findProfessional() {
+    return null;
+  }
+}
