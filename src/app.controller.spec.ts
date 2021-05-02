91be94356ce36b5f752ce0c954907f6dd715349e
ivanilsonAlmeida <ivan.almeidapibane@gmail.com>
2021-05-02 04:05:14 -0300

    adjust test for get status from system

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Back-End working!"', () => {
+      expect(appController.getStatusSystem()).toBe('Back-End working!');
     });
   });
 });
